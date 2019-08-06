66e77ed49482b36d3b8cf2bc2760dd3a634b6048
Dusan Jurica <dusan.jurica@gmail.com>
2019-08-06 13:45:29 +0200

    Do solutionu jsem pridal testproject

---
+++ DusanCompareLibTests/DusanCompareDefinitionTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DusanCompareLib;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace DusanCompareLib.Tests
+{
+    [TestClass()]
+    public class DusanCompareDefinitionTests
+    {
+        public List<string> l1 { get; set; }
+        public List<string> l2 { get; set; }
+
+        public DusanCompareDefinition dComparer { get; set; }
+
+        public DusanCompareDefinitionTests()
+        {
+            Console.WriteLine("Initializing testrunner with data.");
+
+            //l1 = File.ReadAllLines(@"../../lines1.txt").ToList();
+
+            //l2 = File.ReadAllLines(@"../../lines2.txt").ToList();
+
+            dComparer = new DusanCompareDefinition();
+        }
+
+        [TestMethod()]
+        public void GetDifferencesTest_bothSidesAreEqual_expectsEmptyList()
+        {
+            l1 = new List<string>
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            l2 = new List<string>
+            {
+                "a",
+                "b",
+                "c"
+
+            };
+
+            List<string> diffs = new List<string>();
+
+            CollectionAssert.AreEqual(diffs, dComparer.GetDifferences(l1, l2));
+        }
+    }
+}
