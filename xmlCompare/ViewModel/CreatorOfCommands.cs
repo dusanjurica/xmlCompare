b70c75fa13e41a97cbd6f4901b0e5abe45722bde
Dusan Jurica <dusan.jurica@gmail.com>
2019-08-06 09:09:04 +0200

    Initial commit

---
+++ xmlCompare/ViewModel/CreatorOfCommands.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace xmlCompare.ViewModel
+{
+    public class CreatorOfCommands : ICommand
+    {
+        Action _execute;
+        bool _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public CreatorOfCommands(Action execute, bool canExecute)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute();
+        }
+    }
+}
---
+++ xmlCompare/ViewModel/ShellVM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace xmlCompare.ViewModel
+{
+    public class ShellVM
+    {
+        #region LoadFilesCommand
+        private ICommand _loadFilesCommand;
+
+        public ICommand LoadFilesCommand
+        {
+            get
+            {
+                return _loadFilesCommand ??
+                  new CreatorOfCommands(LoadFilesAction, true);
+            }
+        }
+        #endregion
+        #region CompareCommand
+        private ICommand _compareCommand;
+
+        public ICommand CompareCommand
+        {
+            get
+            {
+                return _compareCommand ??
+                  new CreatorOfCommands(CompareAction, true);
+            }
+        }
+        #endregion
+
+        public ObservableCollection<string> differences { get; set; } = new ObservableCollection<string>();
+
+        public ShellVM()
+        {
+        }
+
+        void LoadFilesAction()
+        {
+            Console.WriteLine("Loading files");
+        }
+
+        void CompareAction()
+        {
+            Console.WriteLine("Comparing files");
+
+            differences.Add("diff");
+        }
+    }
+}
